20194c9f617264157e895b7b67b2d69c3641d2a3
tp86 <tom0palka+tp86@gmail.com>
2022-06-17 14:59:41 +0200

    Refactor - more functional approach

--- 2015/5/main.rkt
+++ 2015/5/main.rkt
@@ -1,35 +1,38 @@
 #lang racket
 
+(define (build-matcher rx)
+  (λ (s)
+    (regexp-match? rx s)))
+
+(define (nice? matchers str)
+  (for/and ([f matchers])
+    (f str)))
+
+(define (solve matchers)
+  (for/sum ([s (file->lines "input.txt")])
+    (let ([n (nice? matchers s)])
+      (if n 1 0))))
+
 ; part 1
-(define (has-3-vowels? str)
-  (regexp-match #px"([aeiou].*){2}[aeiou]" str))
+(define has-3-vowels?
+  (build-matcher #px"([aeiou].*){2}[aeiou]"))
 
-(define (has-double-letter? str)
-  (regexp-match #px"(.)\\1" str))
+(define has-double-letter?
+  (build-matcher #px"(.)\\1"))
 
-(define (has-naughty-part? str)
-  (regexp-match #rx"ab|cd|pq|xy" str))
+(define has-naughty-part?
+  (build-matcher #rx"ab|cd|pq|xy"))
 
-(define (nice1? str)
-  (and (has-3-vowels? str)
-       (has-double-letter? str)
-       (not (has-naughty-part? str))))
-
-(for/sum ([s (file->lines "input.txt")])
-  (let ([n (nice1? s)])
-    (if n 1 0)))
+(solve (list has-3-vowels?
+             has-double-letter?
+             (compose not has-naughty-part?)))
 
 ; part 2
-(define (has-pair? str)
-  (regexp-match #px"(..).*\\1" str))
+(define has-pair?
+  (build-matcher #px"(..).*\\1"))
 
-(define (has-one-between? str)
-  (regexp-match #px"(.).\\1" str))
+(define has-one-between?
+  (build-matcher #px"(.).\\1"))
 
-(define (nice2? str)
-  (and (has-pair? str)
-       (has-one-between? str)))
-
-(for/sum ([s (file->lines "input.txt")])
-  (let ([n (nice2? s)])
-    (if n 1 0)))
+(solve (list has-pair?
+             has-one-between?))
