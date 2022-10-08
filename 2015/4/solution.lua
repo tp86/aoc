cd8413393b8cdb5d0ffcb9203b9e6b35914d0be7
tp86 <tom0palka+tp86@gmail.com>
2022-10-08 22:23:37 +0200

    Optimize Day 4 solution

--- 2015/4/solution.lua
+++ 2015/4/solution.lua
@@ -2,18 +2,14 @@ local md5 = require('luazen').md5
 
 local input = 'yzbqklnj'
 
-local function solve(input, n)
-  local bytesneeded = math.ceil(n / 2)
-  local formatstring = string.rep('%02x', bytesneeded)
-  local pattern = '^' .. string.rep('0', n)
+local function solve(input, pattern)
   for i = 1,math.maxinteger do
     local hash  = md5(input .. i)
-    hash = string.format(formatstring, string.byte(hash, 1, bytesneeded))
     if string.match(hash, pattern) then
       return i
     end
   end
 end
 
-print('1', solve(input, 5))
-print('2', solve(input, 6))
+print('1', solve(input, '^\x00\x00[\x00-\x0f]'))
+print('2', solve(input, '^\x00\x00\x00'))
